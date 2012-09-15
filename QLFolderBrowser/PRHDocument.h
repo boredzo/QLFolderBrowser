5006cfa3fb56a4b6667b2b7f2ac1e87d25274efe
Peter Hosey <git@boredzo.org>
2012-09-14 19:34:35 -0700

    Initial Commit

---
+++ QLFolderBrowser/PRHDocument.h
@@ -0,0 +1,13 @@
+//
+//  PRHDocument.h
+//  QLFolderBrowser
+//
+//  Created by Peter Hosey on 2012-09-14.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PRHDocument : NSDocument
+
+@end
